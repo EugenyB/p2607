472b5d1e815786a12afbe4856de42b8c1f64d4a5
eugenyb <eberkunsky@gmail.com>
2023-07-26 19:21:12 +0300

    Initial commit

---
+++ src/main/java/main2/CallableIntegralCalculator.java
@@ -0,0 +1,18 @@
+package main2;
+
+import java.util.concurrent.Callable;
+import java.util.function.DoubleUnaryOperator;
+
+public class CallableIntegralCalculator implements Callable<Double> {
+
+    private final IntegralCalculator calculator;
+
+    public CallableIntegralCalculator(double a, double b, int n, DoubleUnaryOperator f) {
+        calculator = new IntegralCalculator(a, b, n, f);
+    }
+
+    @Override
+    public Double call() throws Exception {
+        return calculator.calculate();
+    }
+}
